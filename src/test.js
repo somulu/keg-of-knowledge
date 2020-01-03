684cf998aaae4cd02223c2c81f83fad4142069d6
Benjamin Griffiths <drjamin1990@gmail.com>
2020-01-03 17:46:37 +0000

    added example commands for api

---
+++ src/test.js
@@ -0,0 +1,14 @@
+const APIkey=e8afe73843413e14dc6a7218b469a279;
+
+// Example Commands
+fetch('https://sandbox-api.brewerydb.com/v2/breweries/?key=e8afe73843413e14dc6a7218b469a279')
+.then(response => response.json())
+.then(beers => {console.log(beers.data)});
+
+fetch('https://sandbox-api.brewerydb.com/v2/beer/c4f2KE/?key=e8afe73843413e14dc6a7218b469a279')
+.then(response => response.json())
+.then(beers => {console.log(beers.data)});
+
+fetch('https://sandbox-api.brewerydb.com/v2/brewery/HaPdSL/beers/?key=e8afe73843413e14dc6a7218b469a279')
+.then(response => response.json())
+.then(beers => {console.log(beers.data)});
